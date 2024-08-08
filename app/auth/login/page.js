14d4ff3e3814ac4677aee0c244243b06605402cc
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2024-08-08 16:55:24 +0330

    Styling the login form

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,20 +1,27 @@
+import Link from "next/link";
 import React from "react";
 
 export default function loginPage() {
   return (
     <div className="flex justify-center items-center h-screen relative">
       <div className="w-full h-full absolute inset-0 z-[-1] bg-gray-50 blur-[120px]">
-        <div className="w-[10rem] h-[10rem] bg-red-c-100 z-0 absolute inset-[5rem]"></div>
-        <div className="w-[10rem] h-[10rem] bg-green-c-100 z-0 absolute right-0 top-[10rem]"></div>
-        <div className="w-[10rem] h-[10rem] bg-beige-c-100 z-0 absolute left-[15rem] bottom-0"></div>
+        <div className="w-[10rem] h-[10rem] bg-beige-c-100 z-0 absolute right-[3rem] top-[10rem]"></div>
+        <div className="w-[10rem] h-[10rem] bg-red-c-100 z-0 absolute left-[20rem] bottom-[10rem]"></div>
+        <div className="w-[10rem] h-[10rem] bg-green-c-100 z-0 absolute left-[7rem] bottom-[2rem]"></div>
       </div>
+
       <div className="sm:w-[30rem] w-[90%] border rounded-lg bg-white p-8">
         <div className="flex min-h-full flex-1 flex-col justify-center">
           <div className="flex items-center justify-between sm:mx-auto sm:w-full sm:max-w-sm">
             <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
               Login
             </h2>
-            <p>Don't have an account?</p>
+            <Link
+              className="text-sm  text-blue-500 hover:text-blue-600"
+              href="/"
+            >
+              Don't have an account?
+            </Link>
           </div>
 
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -22,7 +29,7 @@ export default function loginPage() {
               <div>
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="block text-sm font-medium leading-6 text-gray-500"
                 >
                   Email address
                 </label>
@@ -33,7 +40,7 @@ export default function loginPage() {
                     type="email"
                     required
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full px-3 py-3 rounded-md border-0 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-300 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
@@ -42,14 +49,14 @@ export default function loginPage() {
                 <div className="flex items-center justify-between">
                   <label
                     htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
+                    className="block text-sm font-medium leading-6 text-gray-500"
                   >
                     Password
                   </label>
                   <div className="text-sm">
                     <a
                       href="#"
-                      className="font-semibold text-indigo-600 hover:text-indigo-500"
+                      className="font-semibold text-blue-500 hover:text-blue-700"
                     >
                       Forgot password?
                     </a>
@@ -62,7 +69,7 @@ export default function loginPage() {
                     type="password"
                     required
                     autoComplete="current-password"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full px-3 py-3 rounded-md border-0 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-300 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
@@ -70,22 +77,13 @@ export default function loginPage() {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                 >
                   Sign in
                 </button>
               </div>
             </form>
 
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{" "}
-              <a
-                href="#"
-                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              >
-                Start a 14 day free trial
-              </a>
-            </p>
           </div>
         </div>
       </div>
