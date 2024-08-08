2c18f1b2a4229becdb3952f7a01acfcb789cba18
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2024-08-08 17:35:50 +0330

    change styles background again

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -7,7 +7,7 @@ export default function loginPage() {
 
       <div className="w-full h-full absolute inset-0 z-[-1] bg-gray-50 blur-[120px]">
         <div className="w-[10rem] h-[10rem] bg-beige-c-100 z-0 absolute right-[3rem] top-[10rem]"></div>
-        <div className="w-[30rem] h-[30rem] bg-green-c-100 z-0 absolute left-[10rem] bottom-[10rem]"></div>
+        <div className="w-[17rem] h-[17rem] bg-green-c-100 z-0 absolute left-[10rem] bottom-[10rem]"></div>
         <div className="w-[10rem] h-[10rem] bg-red-c-100 z-0 absolute left-[0rem] bottom-[2rem]"></div>
       </div>
 
