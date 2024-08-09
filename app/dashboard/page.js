2554baf92ea73193b73b5d17a3d3c3aa433e966d
Erphun <heydar858@gmail.com>
2024-08-09 23:37:55 +0330

    Added: Topbar (not completed)

---
+++ app/dashboard/page.js
@@ -0,0 +1,112 @@
+import SidebarProvider from '../_Contexts/SidebarProvider';
+import Sidebar from './_Sidebar/Sidebar';
+import Topbar from './_Topbar/Topbar';
+
+export default function Home() {
+    return (
+        <main className="relative bg-dashboard-bg min-h-screen text-dashboard-text">
+            <SidebarProvider>
+                <Topbar />
+                <Sidebar />
+            </SidebarProvider>
+            <div className="px-4 425:px-6 md:px-8">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                placeat laboriosam totam numquam maiores vitae eligendi, ad
+                consequuntur? Veritatis sint velit at sunt? Iure, minima.
+                Quibusdam cum optio dolor reprehenderit.
+                <br />
+                <br />
+            </div>
+        </main>
+    );
+}
