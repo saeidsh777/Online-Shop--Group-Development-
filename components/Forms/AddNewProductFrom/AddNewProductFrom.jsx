7ee845d68fecb3dd5eefe0bc15306e1b73f0ed84
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2024-08-13 23:56:47 +0330

    CREATE: AddNewProductFrom Component

--- components/Forms/AddNewProductFrom/AddNewProductFrom.jsx
+++ components/Forms/AddNewProductFrom/AddNewProductFrom.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
-
+'use client';
+import Image from 'next/image';
+import { useRef, useState } from 'react';
+import { FiUploadCloud } from 'react-icons/fi';
 export default function AddNewProductFrom() {
+    const [textInputFile, setTextInputFile] = useState('No File');
+    const productImage = useRef();
+
+    // Receiving photos from input file and creating a viewable photo
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            let reader = new FileReader();
+
+            reader.onload = function (e) {
+                productImage.current.setAttribute('src', e.target.result);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
     return (
         <section className="bg-white rounded-xl border-gray-200 border p-6">
             <form className=" grid grid-cols-2 gap-3" name="add-new-product">
@@ -76,15 +94,53 @@ export default function AddNewProductFrom() {
                             </div>
                         </div>
                     </div>
-                    <div className='mt-3'>
-                        <small>Final Price: <span className='ms-2 bg-gray-100 px-2 py-1 rounded-lg'>0$</span></small>
+                    <div className="mt-3">
+                        <small>
+                            Final Price:{' '}
+                            <span className="ms-2 bg-gray-100 px-2 py-1 rounded-lg">
+                                0$
+                            </span>
+                        </small>
                     </div>
                 </div>
 
                 <div className="p-4 border border-gray-200 rounded-xl">
                     <div className="mb-3">
-                        
-                        <input type="file" />
+                        <label
+                            htmlFor="img"
+                            className={`${
+                                textInputFile === 'No File'
+                                    ? 'bg-inherit'
+                                    : 'bg-gradient-to-r from-sky-50 to-indigo-100'
+                            } flex justify-between overflow-hidden hover:bg-gray-50 cursor-pointer items-center border border-gray-200 rounded-md`}
+                        >
+                            <Image
+                                ref={productImage}
+                                src="/images/default-image-product.svg"
+                                width={100}
+                                height={100}
+                                alt="product image"
+                                className={`${
+                                    textInputFile == 'No File'
+                                        ? 'ps-2'
+                                        : 'ps-0 object-cover w-[100px] h-[100px]'
+                                } `}
+                            />
+                            <div className="w-full flex flex-col items-center justify-center">
+                                <FiUploadCloud className="iconFontSize" />
+                                <p>{textInputFile}</p>
+                            </div>
+                            <input
+                                id="img"
+                                type="file"
+                                className="hidden"
+                                onChange={e => {
+                                    readURL(e.target);
+                                    setTextInputFile(e.target.files[0].name);
+                                }}
+                                accept="image/png, image/jpeg, image/jpg"
+                            />
+                        </label>
                     </div>
                     <div className="mb-3">
                         <label className="text-sm" htmlFor="description">
