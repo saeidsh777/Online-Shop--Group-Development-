e5444e12f1a0e131117fcd11d90c21f6d38bb9d1
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2024-08-14 01:39:48 +0330

    EDIT: Use of DashboadBox Component

--- app/dashboard/(products)/add-new-product/page.jsx
+++ app/dashboard/(products)/add-new-product/page.jsx
@@ -1,11 +1,14 @@
-import AddNewProductFrom from "@/components/Forms/AddNewProductFrom/AddNewProductFrom";
-import SectionTitel from "@/components/Titels/SectionTitel/SectionTitel";
+import DashboardBox from '@/components/Boxes/DashboardBox';
+import AddNewProductFrom from '@/components/Forms/AddNewProductFrom/AddNewProductFrom';
+import SectionTitel from '@/components/Titels/SectionTitel/SectionTitel';
 
 export default function addNewProductPage() {
-  return (
-      <div className="mt-5">
-          <SectionTitel title={'Add New Product'} />
-          <AddNewProductFrom/>
-      </div>
-  );
+    return (
+        <div className="mt-5">
+            <SectionTitel title={'Add New Product'} />
+            <DashboardBox>
+                <AddNewProductFrom />
+            </DashboardBox>
+        </div>
+    );
 }
