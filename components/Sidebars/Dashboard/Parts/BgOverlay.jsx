246cf90bbd44e65e3779c591b6151a173f19c5f2
Erphun <heydar858@gmail.com>
2024-08-15 14:31:26 +0330

    Changed: sidebar & bgoverlay z-index

--- components/Sidebars/Dashboard/Parts/BgOverlay.jsx
+++ components/Sidebars/Dashboard/Parts/BgOverlay.jsx
@@ -2,7 +2,7 @@ const BgOverlay = ({ isSidebarActive, CloseSidebar }) => {
     return (
         <div
             className={`fixed w-screen h-screen bg-black/20 top-0 left-0 transition-all duration-200 1152:hidden ${
-                isSidebarActive ? 'z-1' : '-z-[1]'
+                isSidebarActive ? 'z-10' : '-z-[1]'
             }`}
             onClick={CloseSidebar}
         ></div>
