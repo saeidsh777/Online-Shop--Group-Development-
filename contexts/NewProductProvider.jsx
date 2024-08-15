6800f39627280f298310d2feb6c8c9e8e4254d7a
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2024-08-15 11:29:11 +0330

    CREATE: NewProductContext

---
+++ contexts/NewProductProvider.jsx
@@ -0,0 +1,36 @@
+'use client';
+import { createContext, useState } from 'react';
+
+export const NewProductContext = createContext({});
+
+export default function NewProductProvider({ children }) {
+    const [inputs, setInputs] = useState({
+        name: '',
+        category: '',
+        price: "",
+        discountType: '-1',
+        discount: "",
+        finalPrice: 0,
+    });
+
+    const onChange = (fieldName, value) => {
+        setInputs(prv => {
+            return {
+                ...prv,
+                [fieldName]: value,
+            };
+        });
+    };
+
+    const contextValue = {
+        inputs,
+        onChange,
+        setInputs,
+    };
+
+    return (
+        <NewProductContext.Provider value={contextValue}>
+            {children}
+        </NewProductContext.Provider>
+    );
+}
