e270cd5e9fbd2d6f6e66916445d355040d945525
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2024-08-15 11:30:37 +0330

    ADDED: NewProductProvider to addNewProductPage

--- app/dashboard/(products)/add-new-product/page.jsx
+++ app/dashboard/(products)/add-new-product/page.jsx
@@ -1,13 +1,16 @@
 import DashboardBox from '@/components/Boxes/DashboardBox';
 import AddNewProductFrom from '@/components/Forms/AddNewProductFrom/AddNewProductFrom';
 import SectionTitel from '@/components/Titels/SectionTitel/SectionTitel';
+import NewProductProvider from '@/contexts/NewProductProvider';
 
 export default function addNewProductPage() {
     return (
         <div className="mt-5">
             <SectionTitel title={'Add New Product'} />
             <DashboardBox>
-                <AddNewProductFrom />
+                <NewProductProvider>
+                    <AddNewProductFrom />
+                </NewProductProvider>
             </DashboardBox>
         </div>
     );
