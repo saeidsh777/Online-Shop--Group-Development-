04f588ae6be1a70901e9e66d897274e7c4ced4ea
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2024-08-19 14:38:01 +0330

    CREATE: ProductEditPage

---
+++ app/dashboard/(products)/products/[productId]/edit/page.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function ProductEditPage({ params }) {
+    const { productId } = params;
+    return <div>EditP</div>;
+}
--- app/dashboard/product/[productID]/page.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function ProductEditPage({params}) {
-    const {productID} = params;
-  return (
-    <div>EditP</div>
-  )
-}
