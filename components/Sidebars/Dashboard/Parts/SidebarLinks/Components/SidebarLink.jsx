4b41bd7352aa91df9f1b3359dbf264f747b84a4a
Erphun <heydar858@gmail.com>
2024-08-20 23:26:28 +0330

    Added: tooltip hover on dashboard sidebar links in 1152px when sidebar is closed

--- components/Sidebars/Dashboard/Parts/SidebarLinks/Components/SidebarLink.jsx
+++ components/Sidebars/Dashboard/Parts/SidebarLinks/Components/SidebarLink.jsx
@@ -5,7 +5,7 @@ const SidebarLink = ({ href: hrefProps, text, children }) => {
     const href = '/dashboard' + (hrefProps === '/' ? '' : hrefProps);
     return (
         <Link
-            className={`py-2 pl-4 md:py-3 md:pl-5 1152:py-4 1152:px-4 cursor-pointer flex items-center justify-start gap-2 rounded-xl transition-colors duration-300 Dashboard_Sidebar-linkContainer ${
+            className={`py-2 pl-4 md:py-3 md:pl-5 1152:py-4 1152:px-4 cursor-pointer flex items-center justify-start gap-2 rounded-xl group transition-colors duration-300 Dashboard_Sidebar-linkContainer ${
                 IsLinkActive(href)
                     ? 'text-dashboard-sidebar-textActive bg-dashboard-sidebar-bgActive hover:bg-dashboard-sidebar-bgActiveHover'
                     : 'text-dashboard-text hover:bg-dashboard-sidebar-hover'
@@ -13,8 +13,11 @@ const SidebarLink = ({ href: hrefProps, text, children }) => {
             href={href}
         >
             <div className="iconFontSize">{children}</div>
-            <div className="1152:max-w-96 1152:max-h-96 Dashboard_Sidebar-linktext 1152:line-clamp-1">
-                {text}
+            <div className="Dashboard_Sidebar-linktext">
+                <div className="relative">
+                    <div className="text">{text}</div>
+                    <div className="leftArrow max-1152:hidden"></div>
+                </div>
             </div>
         </Link>
     );
