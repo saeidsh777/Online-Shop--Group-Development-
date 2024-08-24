fd4aa9c877f2815b1c5f5c189aa9d0a826e044d1
Erphun <s3niffer@gmail.com>
2024-08-24 13:58:45 +0330

    Added: z-index to dashboard header

--- components/Headers/Dashboard/Header.jsx
+++ components/Headers/Dashboard/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     const { SwitchSidebarStatus } = useContext(SidebarContext);
 
     return (
-        <header className="sticky top-0 left-0 bg-dashboard-bg/80 backdrop-blur-[8px] 1152:row-start-1 1152:row-end-1 1152:col-start-2 1152:col-end-3">
+        <header className="sticky top-0 left-0 z-10 bg-dashboard-bg/80 backdrop-blur-[8px] 1152:row-start-1 1152:row-end-1 1152:col-start-2 1152:col-end-3">
             <div className="mx-auto px-3.5 py-2 sm:py-4 sm:px-7 1152:py-6 flex items-center justify-between">
                 <div
                     className="cursor-pointer bg-dashboard-bg rounded p-2.5  hover:bg-[#dbe0e5] transition-colors"
