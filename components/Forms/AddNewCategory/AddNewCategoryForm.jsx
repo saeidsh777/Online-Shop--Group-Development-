69666f8ee6596125f815ee94c505a4cf61eaf368
Erphun <s3niffer@gmail.com>
2024-08-30 14:41:35 +0330

    Merged: useField into useAddCategory

--- components/Forms/AddNewCategory/AddNewCategoryForm.jsx
+++ components/Forms/AddNewCategory/AddNewCategoryForm.jsx
@@ -1,47 +1,11 @@
 'use client';
-
 import DashboardBTN from '@/components/Buttons/Dashboard/DashboardBTN';
 import DashboardInput from '@/components/Inputs/DashboardInput/DashboardInput';
 import useAddCategory from '@/hooks/useAddCategory';
-import useField from '@/hooks/useField';
-import { useRef } from 'react';
-import toast from 'react-hot-toast';
-import FieldButton from './Components/FieldButton/FieldButton';
-import FieldsBox from './Components/FieldsBox';
 
 const AddNewCategoryForm = () => {
-    const inputRef = useRef(null);
-    const buttonRef = useRef(null);
-    const AddCategory = useAddCategory();
-    const { Fields, FieldsIsActive, Dispatchers } = useField();
+    const { formAction, FieldButton, FieldsBox, Refs } = useAddCategory();
 
-    const formAction = event => {
-        event.preventDefault();
-        if (!inputRef.current) return;
-
-        const title = inputRef.current.value;
-        buttonRef.current.disabled = true;
-
-        // check title
-        if (!title) return;
-        if (title.length < 3 || title.length > 30) {
-            toast.error(
-                'Category name must contain at least 3 and up to 30 maximum charachter'
-            );
-            return;
-        }
-
-        const activeButtonHandler = () => {
-            buttonRef.current.disabled = false;
-        };
-
-        const onSuccess = () => {
-            inputRef.current.value = '';
-            activeButtonHandler();
-        };
-
-        AddCategory(title, onSuccess, activeButtonHandler);
-    };
     return (
         <form
             onSubmit={formAction}
@@ -52,28 +16,14 @@ const AddNewCategoryForm = () => {
                     className="p-3 sm:p-3.5 lg:p-4 lg:px-5 1152:px-6 1152:py-5 flex-1 my-1"
                     placeholder="Category Name..."
                     name="name"
-                    ref={inputRef}
+                    ref={Refs.inputRef}
                 />
 
-                <FieldButton
-                    IsActive={FieldsIsActive}
-                    Add={Dispatchers.AddField}
-                    Remove={Dispatchers.RemoveFields}
-                />
+                {FieldButton}
             </div>
-
-            {FieldsIsActive ? (
-                <FieldsBox
-                    {...{
-                        AddField: Dispatchers.AddField,
-                        fields: Fields,
-                        ...Dispatchers.FieldDispatchers,
-                    }}
-                />
-            ) : null}
-
+            {FieldsBox}
             <DashboardBTN
-                ref={buttonRef}
+                ref={Refs.buttonRef}
                 className="ml-auto disabled:bg-blue-400 disabled:cursor-wait mt-3"
                 type="submit"
             >
--- hooks/useAddCategory.jsx
+++ hooks/useAddCategory.jsx
@@ -1,7 +1,15 @@
+import FieldButtonComponent from '@/components/Forms/AddNewCategory/Components/FieldButton/FieldButton';
+import FieldsBoxComponent from '@/components/Forms/AddNewCategory/Components/FieldsBox';
 import { addCategory as postCategory } from '@/services/categories';
+import { useRef } from 'react';
 import toast from 'react-hot-toast';
+import useField from './useField';
 
 const useAddCategory = () => {
+    const inputRef = useRef(null);
+    const buttonRef = useRef(null);
+    const { Fields, FieldsIsActive, Dispatchers } = useField();
+
     const AddCategory = async (title, onSuccess, onError) => {
         const Token = localStorage.getItem('token');
 
@@ -26,6 +34,60 @@ const useAddCategory = () => {
         toast.error(error_message);
     };
 
-    return AddCategory;
+    const formAction = event => {
+        event.preventDefault();
+        if (!inputRef.current) return;
+
+        const title = inputRef.current.value;
+        buttonRef.current.disabled = true;
+
+        // check title
+        if (!title) return;
+        if (title.length < 3 || title.length > 30) {
+            toast.error(
+                'Category name must contain at least 3 and up to 30 maximum charachter'
+            );
+            return;
+        }
+
+        const activeButtonHandler = () => {
+            buttonRef.current.disabled = false;
+        };
+
+        const onSuccess = () => {
+            inputRef.current.value = '';
+            activeButtonHandler();
+        };
+
+        AddCategory(title, onSuccess, activeButtonHandler);
+    };
+
+    const FieldButton = (
+        <FieldButtonComponent
+            IsActive={FieldsIsActive}
+            Add={Dispatchers.AddField}
+            Remove={Dispatchers.RemoveFields}
+        />
+    );
+
+    const FieldsBox = FieldsIsActive ? (
+        <FieldsBoxComponent
+            {...{
+                AddField: Dispatchers.AddField,
+                fields: Fields,
+                ...Dispatchers.FieldDispatchers,
+            }}
+        />
+    ) : null;
+
+    return {
+        formAction,
+        FieldButton,
+        FieldsBox,
+        Refs: {
+            inputRef,
+            buttonRef,
+        },
+    };
 };
 export default useAddCategory;
