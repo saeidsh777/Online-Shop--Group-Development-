f2975f9142b19f1a2e5ece95ff1d0e5221cd7799
Erphun <s3niffer@gmail.com>
2024-09-02 18:28:39 +0330

    Transfered: ActionIcons to seperate file & Feat: tootip customizable

--- components/Table/DashboardTableRow.jsx
+++ components/Table/DashboardTableRow.jsx
@@ -1,6 +1,7 @@
 import { DeleteWrapper } from '@/hooks/useModal';
 import Image from 'next/image';
 import Link from 'next/link';
+import ActionIcon from './ActionIcons';
 
 const DashboardTableRow = props => {
     const {
@@ -101,42 +102,6 @@ const DashboardTableRow = props => {
                 </div>
             </td>
         </tr>
-    );
-};
-
-const ActionIcon = ({ type, children: SVGsPaths }) => {
-    const types = {
-        view: {
-            bg: 'hover:bg-gray-200',
-            color: 'currentColor',
-        },
-        delete: {
-            bg: 'hover:bg-red-200',
-            color: ' #dc2626',
-        },
-        update: {
-            bg: 'hover:bg-blue-200',
-            color: '#2563eb',
-        },
-    };
-
-    return (
-        <div
-            className={`p-1.5 rounded-lg cursor-pointer relative group ${types[type].bg}`}
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-4 aspect-square md:w-5 lg:w-6"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke={types[type].color}
-            >
-                {SVGsPaths}
-            </svg>
-            <p className="absolute opacity-0 group-hover:opacity-100 z-10 capitalize bottom-[115%] left-1/2 -translate-x-1/2 bg-white shadow-xl p-1 rounded-lg after:content-[''] after:absolute after:-bottom-1 after:left-1/2 after:-m-1 after:border-4 after:border-solid after:border-transparent after:border-t-white ">
-                {type}
-            </p>
-        </div>
     );
 };
 
