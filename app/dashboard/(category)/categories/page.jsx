8fd59667e7e9935f772b462f2f3a0143cb75fe61
Erphun <s3niffer@gmail.com>
2024-09-04 19:41:02 +0330

    Added: filter list logic

--- app/dashboard/(category)/categories/page.jsx
+++ app/dashboard/(category)/categories/page.jsx
@@ -1,19 +1,61 @@
 import CategoryBox from '@/components/Boxes/CategoryBox/CategoryBox';
 import DashboardBox from '@/components/Boxes/DashboardBox';
 import DashboardBTN from '@/components/Buttons/Dashboard/DashboardBTN';
-import DashboardInput from '@/components/Inputs/DashboardInput/DashboardInput';
 import ListIsEmpty from '@/components/Others/ListIsEmpty';
 import SectionTitel from '@/components/Titels/SectionTitel/SectionTitel';
+import FilterList from '@/hooks/FilterList';
 import RefreshPage from '@/hooks/RefreshPage';
 import { getAllCategories } from '@/services/categories';
 import Link from 'next/link';
 
-const page = async () => {
+const page = async ({ searchParams }) => {
     const categories = await getAllCategories();
 
     if (categories.err) return <RefreshPage />;
 
     const { result: Data } = categories;
+
+    const rendredCategories = data =>
+        data.map(category => <CategoryBox key={category.id} {...category} />);
+
+    const filterCategoryHandler = (datas, search) => {
+        const filtredCategory = search
+            ? datas.filter(category =>
+                  category.title.toLowerCase().includes(search.toLowerCase())
+              )
+            : datas;
+
+        if (filtredCategory.length)
+            return (
+                <>
+                    <p>
+                        results found for ‘
+                        <span className="text-black">{search}</span>’:
+                    </p>
+                    {rendredCategories(filtredCategory)}
+                </>
+            );
+
+        return (
+            <div className="text-[110%]">
+                <p>
+                    No results found for ‘
+                    <span className="text-black">{search}</span>’.
+                </p>
+                <p>Try searching with a different name.</p>
+                <p>
+                    Or click{' '}
+                    <Link
+                        className="font-bold underline underline-offset-auto hover:no-underline text-blue-600"
+                        href="/dashboard/categories"
+                    >
+                        here
+                    </Link>{' '}
+                    to reset the search
+                </p>
+            </div>
+        );
+    };
 
     return (
         <div>
@@ -22,12 +64,7 @@ const page = async () => {
                 {Data.length ? (
                     <>
                         <div className="flex sm:items-center justify-between mb-6 flex-col gap-1.5 sm:flex-row">
-                            <div className="w-full max-w-xs 896:max-w-sm">
-                                <DashboardInput
-                                    className="p-2 sm:px-3 md:py-2.5"
-                                    placeholder="Search..."
-                                />
-                            </div>
+                            <FilterList />
                             <Link
                                 href="/dashboard/add-new-category"
                                 className="w-fit"
@@ -38,9 +75,12 @@ const page = async () => {
                             </Link>
                         </div>
                         <div className="flex flex-col gap-2 425:gap-2.5 sm:gap-3 896:gap-3.5">
-                            {Data.map(category => (
-                                <CategoryBox key={category.id} {...category} />
-                            ))}
+                            {searchParams.search
+                                ? filterCategoryHandler(
+                                      Data,
+                                      searchParams.search
+                                  )
+                                : rendredCategories(Data)}
                         </div>
                     </>
                 ) : (
