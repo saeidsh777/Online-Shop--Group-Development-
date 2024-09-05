103c78bea719587c8ebb2218f556d54f4c3516c2
Erphun <s3niffer@gmail.com>
2024-09-05 16:48:17 +0330

    Added: interceptor modal to edit category

---
+++ app/@Modal/dashboard/(.)categories/[categoryID]/page.jsx
@@ -0,0 +1,25 @@
+import EditCategoryForm from '@/components/Forms/EditCategory/EditCategoryForm';
+import InterceptorModalWrapper from '@/components/ModalsWrapper/InterceptorModalWrapper';
+import ModalProvider from '@/contexts/ModalProvider';
+import RefreshPage from '@/hooks/RefreshPage';
+import { getSingleCategory } from '@/services/categories';
+
+const EditCategoryModal = async ({ params: { categoryID } }) => {
+    const Category = await getSingleCategory(categoryID);
+
+    if (typeof Category === 'string' || !Category.response.ok)
+        return (
+            <InterceptorModalWrapper>
+                <RefreshPage />
+            </InterceptorModalWrapper>
+        );
+
+    return (
+        <ModalProvider>
+            <InterceptorModalWrapper>
+                <EditCategoryForm category={Category.result} GoBackOnSuccess />
+            </InterceptorModalWrapper>
+        </ModalProvider>
+    );
+};
+export default EditCategoryModal;
