70f34fcbe96fb01886e6659f1ac84cb73622924b
Erphun <s3niffer@gmail.com>
2024-09-06 21:00:39 +0330

    Transfered: editCategoryModal to categories folder inside category group router (right exact location where its will be used)

---
+++ app/dashboard/(category)/categories/layout.jsx
@@ -0,0 +1,9 @@
+const layout = ({ children, EditCategoryModal }) => {
+    return (
+        <>
+            {EditCategoryModal}
+            {children}
+        </>
+    );
+};
+export default layout;
