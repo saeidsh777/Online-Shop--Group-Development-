242cb1f9221b04b4622bcd3d003e4b01814ab921
Erphun <s3niffer@gmail.com>
2024-09-07 17:34:45 +0330

    Added: addTicket service

---
+++ services/ticket.js
@@ -0,0 +1,18 @@
+import { API_BASE_URL } from '@/utils/constants';
+
+export const addTicket = async (message, token) => {
+    try {
+        const response = await fetch(API_BASE_URL + '/tickets/create-ticket', {
+            method: 'POST',
+            body: JSON.stringify({ message }),
+            headers: {
+                authorization: `Bearer ${token}`,
+                'content-type': 'application/json',
+            },
+        });
+
+        return response;
+    } catch (e) {
+        return e instanceof Error ? e.message : 'Error';
+    }
+};
