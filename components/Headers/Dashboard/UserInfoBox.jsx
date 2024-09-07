25410b0083c2482c8404f983eca8e25570e936f2
Erphun <s3niffer@gmail.com>
2024-09-07 14:28:52 +0330

    Added: button to login automaticly

--- components/Headers/Dashboard/UserInfoBox.jsx
+++ components/Headers/Dashboard/UserInfoBox.jsx
@@ -1,8 +1,10 @@
 'use client';
 
+import DashboardBTN from '@/components/Buttons/Dashboard/DashboardBTN';
 import GetToken from '@/hooks/useToken';
+import { login } from '@/services/auth';
 import { getUserInfo } from '@/services/user';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 
 const UserInfoBox = () => {
@@ -14,54 +16,81 @@ const UserInfoBox = () => {
         role: 'loading',
     });
     const [loading, setLoading] = useState(false);
+    const [isBruh, setBurh] = useState(false);
+
+    const getDataHandler = useCallback(() => {
+        setLoading(true);
+        const Token = GetToken();
+        if (!Token) return setLoading(false);
+
+        const User = async token => {
+            const user = await getUserInfo(token);
+            setLoading(false);
+            if ('res' in user) {
+                if (user.res.status === 200) {
+                    setInfo(user.result);
+                    return;
+                } else {
+                    toast.error(user.result.message);
+                    setInfo('Error');
+
+                    return;
+                }
+            }
+            if (typeof user === 'string') {
+                setInfo('Error');
+                toast.error(user);
+            }
+        };
+
+        User(Token);
+    }, []);
 
     useEffect(() => {
         const _15min = 900_000;
 
-        const getDataHandler = () => {
-            setLoading(true);
-            const Token = GetToken();
-            if (!Token) return setLoading(false);
-
-            const User = async token => {
-                const user = await getUserInfo(token);
-                setLoading(false);
-                if ('res' in user) {
-                    if (user.res.status === 200) {
-                        setInfo(user.result);
-                        return;
-                    } else {
-                        toast.error(user.result.message);
-                        setInfo('Error');
-
-                        return;
-                    }
-                }
-                if (typeof user === 'string') {
-                    setInfo('Error');
-                    toast.error(user);
-                }
-            };
-
-            User(Token);
-        };
         getDataHandler();
         const Interval = setInterval(getDataHandler, _15min);
         return () => {
             clearInterval(Interval);
         };
-    }, [setInfo]);
+    }, [setInfo, getDataHandler]);
     return (
         <div className="flex items-center gap-2">
             {loading ? (
                 'loading'
-            ) : (
+            ) : !isBruh ? (
                 <div className="flex flex-col">
                     <span className="text-dashboard-title capitalize font-medium">
-                        {info?.name ? info.name : 'BRUH'}
+                        {info?.name ? info.name : setBurh(true)}
                     </span>
-                    <span>{info?.phoneNumber ? info.phoneNumber : 'BRUH'}</span>
+                    <span>
+                        {info?.phoneNumber ? info.phoneNumber : setBurh(true)}
+                    </span>
                 </div>
+            ) : (
+                <DashboardBTN
+                    onClick={async () => {
+                        const data = {
+                            phoneNumber: '09123456789',
+                            password: 'admin1234',
+                        };
+                        const { res, result, err } = await login(data);
+
+                        if (res.status === 200) {
+                            toast.success(result.message);
+                            localStorage.setItem('token', result.token);
+                            setBurh(false);
+                            getDataHandler();
+                        } else if (res.status === 500) {
+                            toast.error(err + '!');
+                        } else {
+                            toast.error(result.message + '!');
+                        }
+                    }}
+                >
+                    Login
+                </DashboardBTN>
             )}
 
             {/* replace it with avatar image */}
