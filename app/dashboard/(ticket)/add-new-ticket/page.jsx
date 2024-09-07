b8f96369c685d5e9236e39921a8d4cac0dcb469c
Erphun <s3niffer@gmail.com>
2024-09-07 17:35:27 +0330

    Added: add new ticket layout & formAction

--- app/dashboard/(ticket)/add-new-ticket/page.jsx
+++ app/dashboard/(ticket)/add-new-ticket/page.jsx
@@ -1,4 +1,32 @@
+import DashboardBox from '@/components/Boxes/DashboardBox';
+import AddNewTicketForm from '@/components/Forms/AddNewTicket/AddNewTicketForm';
+import SectionTitel from '@/components/Titels/SectionTitel/SectionTitel';
+import { addTicket } from '@/services/ticket';
+
 const AddNewTicket = () => {
-    return <div>AddNewTicket</div>;
+    const formAction = async ({ message: Tmessage, token }) => {
+        'use server';
+
+        const response = await addTicket(Tmessage, token);
+
+        if (response.ok) {
+            return true;
+        }
+
+        if (typeof response === 'string') return response;
+        const { message } = await response.json();
+        const error_message =
+            typeof message === 'string' ? message : message[0];
+        return error_message;
+    };
+
+    return (
+        <div>
+            <SectionTitel title={'Add New Ticket'} />
+            <DashboardBox>
+                <AddNewTicketForm formAction={formAction} />
+            </DashboardBox>
+        </div>
+    );
 };
 export default AddNewTicket;
