bef0d40c2a3bd6743c89be7f5c4d441404022a28
Erphun <s3niffer@gmail.com>
2024-09-08 16:38:25 +0330

    Added: getMyTickets service

--- services/ticket.js
+++ services/ticket.js
@@ -16,3 +16,19 @@ export const addTicket = async (message, token) => {
         return e instanceof Error ? e.message : 'Error';
     }
 };
+
+export const getMyTickets = async token => {
+    try {
+        const response = await fetch(API_BASE_URL + '/tickets/my-tickets', {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${token}`,
+                'content-type': 'application/json',
+            },
+        });
+
+        return response;
+    } catch (e) {
+        return e instanceof Error ? e.message : 'Error';
+    }
+};
