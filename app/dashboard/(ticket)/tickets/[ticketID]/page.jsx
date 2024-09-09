6de93ae9992b24d81011e17bedf08a14458100e3
Erphun <s3niffer@gmail.com>
2024-09-09 12:49:05 +0330

    Added: specefic page ticket for tickets page routes

---
+++ app/dashboard/(ticket)/tickets/[ticketID]/page.jsx
@@ -0,0 +1,16 @@
+import DashboardBox from '@/components/Boxes/DashboardBox';
+import TicketBox from '@/components/Boxes/TicketBox.jsx/TicketBox';
+import SectionTitel from '@/components/Titels/SectionTitel/SectionTitel';
+
+const TicketRoom = ({ params: { ticketID } }) => {
+    return (
+        <div>
+            <SectionTitel title="Ticket" />
+            <DashboardBox>
+                <TicketBox ticketID={ticketID} />
+            </DashboardBox>
+        </div>
+    );
+};
+
+export default TicketRoom;
