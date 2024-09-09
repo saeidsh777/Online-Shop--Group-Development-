10da261fa59fd04bb1b6180f7cc02db846fc4f56
Erphun <s3niffer@gmail.com>
2024-09-09 12:08:09 +0330

    Changed: chat message box style

--- components/Boxes/TicketBox.jsx/TicketBox.jsx
+++ components/Boxes/TicketBox.jsx/TicketBox.jsx
@@ -10,23 +10,47 @@ import DashboardBox from '../DashboardBox';
 
 const MessageBox = props => {
     const { message, from, messageData } = props;
-    const formatedDate = useMemo(() => {
-        return new Date(messageData).toDateString();
-    }, [messageData]);
+    // const formatedDate = useMemo(() => {
+    //     return new Date(messageData).toDateString();
+    // }, [messageData]);
 
     return (
         <div
-            className={`p-2 md:p-3 lg:p-3.5 shadow  rounded-lg  425:max-w-[65%] md:max-w-[50%]  w-full   ${
+            className={`425:max-w-[65%] md:max-w-[50%] w-full grid grid-cols-[6px,1fr] ${
                 from === 'USER'
-                    ? 'text-white bg-dashboard-sidebar-textActive mr-auto rounded-bl-none flex flex-col'
-                    : 'text-dashboard-title bg-white ml-auto rounded-br-none'
+                    ? 'grid-cols-[6px,1fr] mr-auto'
+                    : 'grid-cols-[1fr,6px] ml-auto'
             }`}
         >
-            <p className="font-extralight">
-                {from === 'USER' ? 'Me' : 'Admin'}:
-            </p>
-            {message}
-            <div className="mt-1 ml-auto w-fit text-[75%]">{formatedDate}</div>
+            <div
+                className={`w-full h-full relative before:absolute before:content-[''] before:block before:w-[200%] before:h-full before:rounded-xl before:bg-dashboard-sidebar-hover grid grid-rows-[5fr,1fr] order-2 ${
+                    from === 'USER'
+                        ? 'before:right-0 order-1'
+                        : 'before:left-0 order-2'
+                }`}
+            >
+                <div></div>
+                <div
+                    className={
+                        from === 'USER'
+                            ? 'bg-dashboard-sidebar-textActive'
+                            : 'bg-white '
+                    }
+                ></div>
+            </div>
+            <div
+                className={`p-2 md:p-3 lg:p-3.5 rounded-lg w-full drop-shadow-lg  ${
+                    from === 'USER'
+                        ? 'text-white bg-dashboard-sidebar-textActive rounded-bl-none flex flex-col order-2'
+                        : 'text-dashboard-title bg-white rounded-br-none order-1'
+                }`}
+            >
+                <p className="font-extralight">
+                    {from === 'USER' ? 'Me' : 'Admin'}:
+                </p>
+                {message}
+                {/* <div className="mt-1 ml-auto w-fit text-[75%]">{formatedDate}</div> */}
+            </div>
         </div>
     );
 };
@@ -66,18 +90,17 @@ const TicketBox = ({ ticketID }) => {
                 Created at:{' '}
                 <span className="font-medium">{createDateFormater}</span>
             </p>
-            {/* <DashboardBox className="border-dashed bg-dashboard-sidebar-hover flex flex-col gap-1 max-h-80 overflow-y-auto">
-                {state.messages.map(message => (
-                    <MessageBox key={message._id} message="heelo" from="USER" />
-                ))}
-                <MessageBox message="heelo" from="ADMIN" />
+            <DashboardBox className="border-dashed flex flex-col gap-1 max-h-80 overflow-y-auto !bg-dashboard-sidebar-hover">
+                {/* {state.messages.map(message => (
+                    <MessageBox key={message._id} {} message="heelo" from="USER" />
+                ))} */}
                 <MessageBox message="heelo" from="USER" />
                 <MessageBox message="heelo" from="ADMIN" />
                 <MessageBox message="heelo" from="USER" />
                 <MessageBox message="heelo" from="ADMIN" />
                 <MessageBox message="heelo" from="USER" />
                 <MessageBox message="heelo" from="ADMIN" />
-            </DashboardBox> */}
+            </DashboardBox>
             <form className="w-full flex items-center gap-2">
                 <DashboardInput
                     className="p-2 sm:px-3 md:py-2.5"
