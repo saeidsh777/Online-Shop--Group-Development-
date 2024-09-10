3afeb579bac5aefb49fbd35904cdc1cbc14aa749
Erphun <s3niffer@gmail.com>
2024-09-10 22:35:04 +0330

    Refactored: UserInfoBox with AuthContext

--- components/Headers/Dashboard/UserInfoBox.jsx
+++ components/Headers/Dashboard/UserInfoBox.jsx
@@ -1,98 +1,42 @@
 'use client';
 
 import DashboardBTN from '@/components/Buttons/Dashboard/DashboardBTN';
-import GetToken from '@/hooks/useToken';
+import { AuthContext } from '@/contexts/AuthProvider';
 import { login } from '@/services/auth';
-import { getUserInfo } from '@/services/user';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useContext } from 'react';
 import toast from 'react-hot-toast';
 
 const UserInfoBox = () => {
-    const [info, setInfo] = useState({
-        createdAt: 'loading',
-        email: 'loading',
-        name: 'loading',
-        phoneNumber: 'loading',
-        role: 'loading',
-    });
-    const [loading, setLoading] = useState(false);
-    const [isBruh, setBurh] = useState(false);
+    const { User, Handlers } = useContext(AuthContext);
 
-    const getDataHandler = useCallback(() => {
-        setLoading(true);
-        const Token = GetToken();
-        if (!Token) {
-            setLoading(false);
-            setBurh(true);
-            return;
+    const LoginWithAdminCredntials = useCallback(async () => {
+        const adminAuthInfo = {
+            phoneNumber: '09123456789',
+            password: 'admin1234',
+        };
+        const { res, result, err } = await login(adminAuthInfo);
+
+        if (res.status === 200) {
+            toast.success(result.message);
+            Handlers.LoginHandler(result.token);
+        } else if (res.status === 500) {
+            toast.error(err + '!');
+        } else {
+            toast.error(result.message + '!');
         }
+    }, [Handlers]);
 
-        const User = async token => {
-            const user = await getUserInfo(token);
-            setLoading(false);
-            if ('res' in user) {
-                if (user.res.status === 200) {
-                    setInfo(user.result);
-                    return;
-                } else {
-                    toast.error(user.result.message);
-                    setInfo('Error');
-
-                    return;
-                }
-            }
-            if (typeof user === 'string') {
-                setInfo('Error');
-                toast.error(user);
-            }
-        };
-
-        User(Token);
-    }, []);
-
-    useEffect(() => {
-        const _15min = 900_000;
-
-        getDataHandler();
-        const Interval = setInterval(getDataHandler, _15min);
-        return () => {
-            clearInterval(Interval);
-        };
-    }, [setInfo, getDataHandler]);
     return (
         <div className="flex items-center gap-2">
-            {loading ? (
-                'loading'
-            ) : !isBruh ? (
+            {User.isLoggedIn ? (
                 <div className="flex flex-col">
                     <span className="text-dashboard-title capitalize font-medium">
-                        {info?.name ? info.name : setBurh(true)}
+                        {User.details.phoneNumber}
                     </span>
-                    <span>
-                        {info?.phoneNumber ? info.phoneNumber : setBurh(true)}
-                    </span>
+                    <span>{User.details.role}</span>
                 </div>
             ) : (
-                <DashboardBTN
-                    onClick={async () => {
-                        const data = {
-                            phoneNumber: '09123456789',
-                            password: 'admin1234',
-                        };
-                        const { res, result, err } = await login(data);
-
-                        if (res.status === 200) {
-                            toast.success(result.message);
-                            localStorage.setItem('token', result.token);
-                            setBurh(false);
-                            getDataHandler();
-                        } else if (res.status === 500) {
-                            toast.error(err + '!');
-                        } else {
-                            toast.error(result.message + '!');
-                        }
-                    }}
-                >
+                <DashboardBTN onClick={LoginWithAdminCredntials}>
                     Login
                 </DashboardBTN>
             )}
