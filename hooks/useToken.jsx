14b5b8ee5122bcbf3ba1716ece1c5e59d764c63a
Erphun <s3niffer@gmail.com>
2024-09-11 22:47:07 +0330

    Refactored: useToken with authContext

--- hooks/useToken.jsx
+++ hooks/useToken.jsx
@@ -1,9 +1,15 @@
 'use client';
 
+import { AuthContext } from '@/contexts/AuthProvider';
+import { useContext, useMemo } from 'react';
 import toast from 'react-hot-toast';
 
 const useToken = () => {
-    const Token = localStorage.getItem('token');
+    const {
+        User: { token },
+    } = useContext(AuthContext);
+
+    const Token = useMemo(() => token, [token]);
 
     if (!Token) {
         toast.error('Token not found please login/register first');
