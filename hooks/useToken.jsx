ba3ef10608ce119af74341282a53daa68d5b9519
Erphun <s3niffer@gmail.com>
2024-09-12 21:36:48 +0330

    Revert "Refactored: useToken with authContext"
    This reverts commit 14b5b8ee5122bcbf3ba1716ece1c5e59d764c63a.

--- hooks/useToken.jsx
+++ hooks/useToken.jsx
@@ -1,15 +1,9 @@
 'use client';
 
-import { AuthContext } from '@/contexts/AuthProvider';
-import { useContext, useMemo } from 'react';
 import toast from 'react-hot-toast';
 
 const useToken = () => {
-    const {
-        User: { token },
-    } = useContext(AuthContext);
-
-    const Token = useMemo(() => token, [token]);
+    const Token = localStorage.getItem('token');
 
     if (!Token) {
         toast.error('Token not found please login/register first');
