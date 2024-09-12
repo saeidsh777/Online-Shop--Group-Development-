0ddf10059ed11f18400eac01c246f333814dbf41
Erphun <s3niffer@gmail.com>
2024-09-12 22:13:07 +0330

    Revert "Added: privateRoute Component to Dashboard layout"
    This reverts commit cf17eb597a54f0a58b64e5f2e45dfca19a29c3fe.

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,25 +1,22 @@
 import BreadCrumb from '@/components/Breadcrumbs/DashboardBD';
 import ModalProvider from '@/contexts/ModalProvider';
-import PrivateRoute from '@/hooks/PrivateRoute';
 import Header from '../../components/Headers/Dashboard/Header';
 import Sidebar from '../../components/Sidebars/Dashboard/Sidebar';
 import SidebarProvider from '../../contexts/SidebarProvider';
 
 export default function DashboardLayout({ children }) {
     return (
-        <PrivateRoute>
-            <ModalProvider>
-                <main className="relative bg-dashboard-bg min-h-screen text-dashboard-text 1152:grid 1152:grid-rows-[auto,1fr] 1152:grid-cols-[auto,1fr]">
-                    <SidebarProvider>
-                        <Header />
-                        <Sidebar />
-                    </SidebarProvider>
-                    <div className="1152:row-start-2 1152:row-end-3 1152:col-start-2 1152:col-end-3 px-4 425:px-6 md:px-8 pb-3">
-                        <BreadCrumb />
-                        {children}
-                    </div>
-                </main>
-            </ModalProvider>
-        </PrivateRoute>
+        <ModalProvider>
+            <main className="relative bg-dashboard-bg min-h-screen text-dashboard-text 1152:grid 1152:grid-rows-[auto,1fr] 1152:grid-cols-[auto,1fr]">
+                <SidebarProvider>
+                    <Header />
+                    <Sidebar />
+                </SidebarProvider>
+                <div className="1152:row-start-2 1152:row-end-3 1152:col-start-2 1152:col-end-3 px-4 425:px-6 md:px-8 pb-3">
+                    <BreadCrumb />
+                    {children}
+                </div>
+            </main>
+        </ModalProvider>
     );
 }
