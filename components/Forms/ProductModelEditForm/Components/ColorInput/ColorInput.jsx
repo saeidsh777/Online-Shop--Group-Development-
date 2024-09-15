30a2a8523c49146ba8297f673f90a13af14ff101
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2024-09-16 00:32:39 +0330

    CHORE: created color input for product model edit

---
+++ components/Forms/ProductModelEditForm/Components/ColorInput/ColorInput.jsx
@@ -0,0 +1,36 @@
+export default function ColorInput({ name, value, setData, _id }) {
+    const newId = crypto.randomUUID();
+
+    const onChange = e => {
+        setData(prv => {
+            return {
+                ...prv,
+                fields: prv.fields.map(field => {
+                    if (field._id !== _id) return field;
+
+                    return {
+                        ...field,
+                        value: e.target.value,
+                    };
+                }),
+            };
+        });
+    };
+
+    return (
+        <div className="flex items-center gap-2 mb-2">
+            <label htmlFor={name + newId} className="text-md">
+                Color:
+            </label>
+            <div>
+                <input
+                    id={name + newId}
+                    type="color"
+                    value={value}
+                    onChange={onChange}
+                    className="General_Input_1 w-20"
+                />
+            </div>
+        </div>
+    );
+}
