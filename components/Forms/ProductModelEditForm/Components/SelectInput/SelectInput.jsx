4cb08c2c23c4b7d86647ffce3ef3117891a9a4da
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2024-09-16 00:32:24 +0330

    CHORE: created select input for product model edit

---
+++ components/Forms/ProductModelEditForm/Components/SelectInput/SelectInput.jsx
@@ -0,0 +1,46 @@
+'use client';
+export default function SelectInput({
+    name,
+    variantOptions,
+    value,
+    _id,
+    setData,
+}) {
+    const onChange = e => {
+        setData(prv => {
+            return {
+                ...prv,
+                fields: prv.fields.map(field => {
+                    if (field._id !== _id) return field;
+                    return {
+                        ...field,
+                        value: e.target.value,
+                    };
+                }),
+            };
+        });
+    };
+    return (
+        <div className="flex items-center gap-2 mb-2">
+            <label htmlFor={name + _id} className="text-md">
+                {name}:
+            </label>
+            <div>
+                <select
+                    name={name}
+                    id={name + _id}
+                    value={value}
+                    onChange={onChange}
+                    className={`General_Input_1`}
+                >
+                    <option value="-1">select {name}</option>
+                    {variantOptions.map(option => (
+                        <option value={option} key={crypto.randomUUID()}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
+}
