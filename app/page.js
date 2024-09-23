965bbb3cdc018b1f76dbf1ab8d6dab2ee98c4835
Erphun <s3niffer@gmail.com>
2024-09-23 16:32:07 +0330

    Added: go to dashboard button link in home page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+import DashboardBTN from '@/components/Buttons/Dashboard/DashboardBTN';
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <main className="">
-      
-    </main>
-  );
+    return (
+        <main className="flex items-center justify-evenly flex-wrap min-h-dvh">
+            <Link href={'/dashboard'}>
+                <DashboardBTN>Go to Dashboard</DashboardBTN>
+            </Link>
+        </main>
+    );
 }
