c3eec7c81e3b74ee9f576d8198706744fed38a8a
Erphun <s3niffer@gmail.com>
2024-09-23 16:31:51 +0330

    feat: now forbidden sidebar links are invisible for user role

--- components/Sidebars/Dashboard/Parts/SidebarLinks/SidebarLinks.jsx
+++ components/Sidebars/Dashboard/Parts/SidebarLinks/SidebarLinks.jsx
@@ -1,3 +1,6 @@
+import PrivateRoleHandler, {
+    Roles,
+} from '@/components/Wrappers/PrivateRoleHandler';
 import { AiOutlineContainer } from 'react-icons/ai';
 import { BsBoxes, BsEnvelopePaper } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineCategory } from 'react-icons/md';
@@ -14,33 +17,38 @@ import SidebarLinkSection from './Components/SidebarLinkSection';
 const SidebarLinks = () => {
     return (
         <div className="my-1 flex flex-col gap-2.5 overflow-y-auto 1152:overflow-x-hidden px-4 Dashboard_Sidebar-SidebarLinks">
-            <SidebarLink href={'/'} text={'Overview'}>
-                <TbGraph />
-            </SidebarLink>
-            <SidebarLinkSection title={'products'}>
-                <SidebarLink href={'/add-new-product'} text={'Add product'}>
-                    <MdOutlineAddBox />
+            <PrivateRoleHandler minimum_Role={Roles.admin}>
+                <SidebarLink href={'/'} text={'Overview'}>
+                    <TbGraph />
                 </SidebarLink>
-                <SidebarLink href={'/products'} text={'Product list'}>
-                    <BsBoxes />
-                </SidebarLink>
-            </SidebarLinkSection>
-            <SidebarLinkSection title={'category'}>
-                <SidebarLink href={'/add-new-category'} text={'Add category'}>
-                    <TbCategoryPlus />
-                </SidebarLink>
-                <SidebarLink href={'/categories'} text={'Category list'}>
-                    <MdOutlineCategory />
-                </SidebarLink>
-            </SidebarLinkSection>
-            <SidebarLinkSection title={'brand'}>
-                <SidebarLink href={'/add-new-brand'} text={'Add brand'}>
-                    <TbHexagonalPrismPlus />
-                </SidebarLink>
-                <SidebarLink href={'/brands'} text={'Brand list'}>
-                    <TbHexagons />
-                </SidebarLink>
-            </SidebarLinkSection>
+                <SidebarLinkSection title={'products'}>
+                    <SidebarLink href={'/add-new-product'} text={'Add product'}>
+                        <MdOutlineAddBox />
+                    </SidebarLink>
+                    <SidebarLink href={'/products'} text={'Product list'}>
+                        <BsBoxes />
+                    </SidebarLink>
+                </SidebarLinkSection>
+                <SidebarLinkSection title={'category'}>
+                    <SidebarLink
+                        href={'/add-new-category'}
+                        text={'Add category'}
+                    >
+                        <TbCategoryPlus />
+                    </SidebarLink>
+                    <SidebarLink href={'/categories'} text={'Category list'}>
+                        <MdOutlineCategory />
+                    </SidebarLink>
+                </SidebarLinkSection>
+                <SidebarLinkSection title={'brand'}>
+                    <SidebarLink href={'/add-new-brand'} text={'Add brand'}>
+                        <TbHexagonalPrismPlus />
+                    </SidebarLink>
+                    <SidebarLink href={'/brands'} text={'Brand list'}>
+                        <TbHexagons />
+                    </SidebarLink>
+                </SidebarLinkSection>
+            </PrivateRoleHandler>
             <SidebarLinkSection title={'ticket'}>
                 <SidebarLink href={'/create-new-ticket'} text={'Create ticket'}>
                     <TbMailPlus />
