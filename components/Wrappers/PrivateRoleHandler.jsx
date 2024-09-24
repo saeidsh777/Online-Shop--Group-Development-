29bc7485c1fbb4ddfbf71ebfab6499f904ce1bea
Erphun <s3niffer@gmail.com>
2024-09-24 20:15:04 +0330

    Feat: now can pass onlyRole props to PrivateRoleHandler to show the children for only a specific role

--- components/Wrappers/PrivateRoleHandler.jsx
+++ components/Wrappers/PrivateRoleHandler.jsx
@@ -4,12 +4,17 @@ import { AuthContext } from '@/contexts/AuthProvider';
 import { Roles } from '@/utils/Roles';
 import { useContext } from 'react';
 
-const PrivateRoleHandler = ({ children, minimum_Role = 0 }) => {
+const PrivateRoleHandler = ({ children, minimum_Role = 0, onlyRole }) => {
     const { User } = useContext(AuthContext);
 
-    if (Roles[User.details.role] >= minimum_Role) {
-        return children;
+    if (onlyRole !== undefined) {
+        if (Roles[User.details.role] === onlyRole) return children;
+
+        return null;
+    } else {
+        if (Roles[User.details.role] >= minimum_Role) return children;
+
+        return null;
     }
-    return null;
 };
 export default PrivateRoleHandler;
