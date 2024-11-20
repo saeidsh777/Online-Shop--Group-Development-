3e9ca8101590d385d9c9f3267853355e8a82eabe
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2024-11-20 16:17:53 +0330

    FEAT: Create dashboard overview

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,107 +1,53 @@
+import CountBox from '@/components/Boxes/CountBox/CountBox';
+import { LuUsers } from 'react-icons/lu';
+import { BsTicketPerforated } from 'react-icons/bs';
+import { AiOutlineProduct } from 'react-icons/ai';
+import { MdOutlineNotificationsActive } from 'react-icons/md';
 const DashboardHomepage = () => {
     return (
         <div>
-            {/* remove this tag below this just for emulating the scroll */}
-            <div>
-                <h1 className="text-center text-4xl mb-10 font-bold">
-                    overview
-                </h1>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                placeat laboriosam totam numquam maiores vitae eligendi, ad
-                consequuntur? Veritatis sint velit at sunt? Iure, minima.
-                Quibusdam cum optio dolor reprehenderit.
-                <br />
-                <br />
+            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 mb-8">
+                <CountBox
+                    imgSrc="/images/user-icon.svg"
+                    title="Users"
+                    count={'500'}
+                    icon={<LuUsers />}
+                    hrefDir="/dashboard/users"
+                    bg="bg-green-50"
+                    textColer=" text-green-500"
+                />
+                <CountBox
+                    imgSrc="/images/ticket-icon.svg"
+                    title="Tickets"
+                    count={'1000'}
+                    icon={<BsTicketPerforated />}
+                    hrefDir="/dashboard/tickets"
+                    bg="bg-cyan-50"
+                    textColer="text-cyan-500"
+                />
+                <CountBox
+                    imgSrc="/images/product-icon.svg"
+                    title="Products"
+                    count={'3700'}
+                    icon={<AiOutlineProduct />}
+                    hrefDir="/dashboard/products"
+                    bg="bg-yellow-50"
+                    textColer="text-yellow-500"
+                />
+                <CountBox
+                    imgSrc="/images/notification-icon.svg"
+                    title="Notifications"
+                    count={'500'}
+                    icon={<MdOutlineNotificationsActive />}
+                    hrefDir="/dashboard/notification-list"
+                    bg="bg-red-50"
+                    textColer="text-red-500"
+                />
+            </div>
+            <div className="grid grid-cols-3">
+                <div className="bg-white p-4 rounded-lg h-[20rem] col-span-2">
+                    <h2>Views</h2>
+                </div>
             </div>
         </div>
     );
---
+++ components/Boxes/CountBox/CountBox.jsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { IoIosArrowRoundForward } from 'react-icons/io';
+export default function CountBox({
+    imgSrc,
+    title,
+    count,
+    icon,
+    hrefDir,
+    bg,
+    textColer,
+}) {
+    return (
+        <div className="bg-white p-4 rounded-lg border border-gray-200 relative overflow-hidden">
+            <Image
+                className="absolute -bottom-2 right-0 w-[6rem] h-[6rem]"
+                src={imgSrc}
+                alt="users image"
+                width={200}
+                height={200}
+            />
+            <div className="relative">
+                <div className="flex items-center justify-between gap-2">
+                    <div className="flex items-center gap-2 border-b pb-2 pe-4 border-gray-100">
+                        <span
+                            className={`w-10 h-10 flex justify-center items-center ${bg} ${textColer} rounded-lg`}
+                        >
+                            {icon}
+                        </span>
+                        <p className="text-black font-bold ">{title}</p>
+                    </div>
+                    <div className="pb-2">
+                        <Link
+                            className="p-1 rounded-md bg-gray-100 block hover:bg-gray-200"
+                            href={hrefDir}
+                        >
+                            <IoIosArrowRoundForward />
+                        </Link>
+                    </div>
+                </div>
+                <div className="flex items-center py-5">
+                    <p className="text-4xl font-bold">+ {count}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
