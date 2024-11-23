fd049db45da17f47abaea83d71946bde4b08c433
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2024-11-23 12:56:44 +0330

    CHORE:Edited minimim role to display sidebar menu

--- components/Sidebars/Dashboard/Parts/SidebarLinks/SidebarLinks.jsx
+++ components/Sidebars/Dashboard/Parts/SidebarLinks/SidebarLinks.jsx
@@ -21,10 +21,12 @@ import SidebarLinkSection from './Components/SidebarLinkSection';
 const SidebarLinks = () => {
     return (
         <div className="my-1 flex flex-col gap-2.5 overflow-y-auto 1152:overflow-x-hidden px-4 Dashboard_Sidebar-SidebarLinks">
-            <PrivateRoleHandler minimum_Role={Roles.admin}>
+            <PrivateRoleHandler minimum_Role={Roles.user}>
                 <SidebarLink href={'/'} text={'Overview'}>
                     <TbGraph />
                 </SidebarLink>
+            </PrivateRoleHandler>
+            <PrivateRoleHandler minimum_Role={Roles.admin}>
                 <SidebarLinkSection title={'products'}>
                     <SidebarLink href={'/add-new-product'} text={'Add product'}>
                         <MdOutlineAddBox />
