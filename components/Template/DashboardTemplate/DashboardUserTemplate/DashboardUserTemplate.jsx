6982f277b1a452e513d2ab809bfce7aa2f7c8307
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2024-11-23 13:03:18 +0330

    FEAT: Create DashboardUserTemplate

--- components/Template/DashboardTemplate/DashboardUserTemplate/DashboardUserTemplate.jsx
+++ components/Template/DashboardTemplate/DashboardUserTemplate/DashboardUserTemplate.jsx
@@ -1,5 +1,20 @@
 'use client';
 
+import CountBox from "@/components/Boxes/CountBox/CountBox";
+import { BsTicketPerforated } from "react-icons/bs";
+
 export default function DashboardUserTemplate() {
-    return <div>DashboardUserTemplate</div>;
+    return (
+        <div className="grid grid-cols-2">
+            <CountBox
+                imgSrc="/images/ticket-icon.svg"
+                title="Tickets"
+                count={"232"}
+                icon={<BsTicketPerforated />}
+                hrefDir="/dashboard/my-tickets"
+                bg="bg-cyan-50"
+                textColer="text-cyan-500"
+            />
+        </div>
+    );
 }
